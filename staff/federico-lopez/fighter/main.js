72cf32180abaee5ebe2ffa935b4d387b56168efb
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-07-06 16:50:17 +0200

    comments in fighter main.js

--- staff/federico-lopez/fighter/main.js
+++ staff/federico-lopez/fighter/main.js
@@ -1,12 +1,21 @@
 console.log('%cFighter🔥%cv0.0', 'color: red; font-size: 32px;', 'color: dodgerblue;')
 
 var fighter = document.querySelector('.fighter')
+// Declare 'fighter' variable and assign to it the HTML element with attribute class 'fighter'
+
 var oponent = document.querySelector('.oponent')
+// Declara una variable 'oponent' le asignamos el valor que resulta de buscar dentro del document un elemento HTML cuyo atributo clase sea igual a "oponent"
 
 fighter.x = 200
+// Creamos la propiedad 'x' del elemento HTML 'fighter' le asignamos el valor de 200
+
 fighter.width = 50
 fighter.score = 0
+// Creamos la propiedad 'score' del elemento HTML referenciado en la variable 'fighter' y le asigamos un valor de 0
+
 fighter.style.left = fighter.x + 'px'
+// A la propiedad left, de la propiedad style, del elemento HTML 'fighter' le asignamos el valor que resultad de:
+// concatenar el valor de la propiedad 'x' de la variable 'fighter' y el string 'px'
 
 oponent.x = 500
 oponent.width = 50
