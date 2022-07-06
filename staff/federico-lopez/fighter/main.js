501cdec83f7f3d80df101fb152aedb31e8d98ab8
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-07-06 17:54:21 +0200

    add up and down movements for fighter

--- staff/federico-lopez/fighter/main.js
+++ staff/federico-lopez/fighter/main.js
@@ -7,6 +7,8 @@ var oponent = document.querySelector('.oponent')
 // Declara una variable 'oponent' le asignamos el valor que resulta de buscar dentro del document un elemento HTML cuyo atributo clase sea igual a "oponent"
 
 fighter.x = 200
+fighter.y = 0
+
 // Creamos la propiedad 'x' del elemento HTML 'fighter' le asignamos el valor de 200
 
 fighter.width = 50
@@ -14,6 +16,8 @@ fighter.score = 0
 // Creamos la propiedad 'score' del elemento HTML referenciado en la variable 'fighter' y le asigamos un valor de 0
 
 fighter.style.left = fighter.x + 'px'
+fighter.style.top = fighter.y + 'px'
+
 // A la propiedad left, de la propiedad style, del elemento HTML 'fighter' le asignamos el valor que resultad de:
 // concatenar el valor de la propiedad 'x' de la variable 'fighter' y el string 'px'
 
@@ -29,9 +33,15 @@ document.onkeydown = function(event) {
         fighter.x = fighter.x + 10
     } else if (event.key === 'ArrowLeft') {
         fighter.x = fighter.x - 10
+    } else if (event.key === 'ArrowUp') {
+        fighter.y = fighter.y - 10
+    } else if (event.key === 'ArrowDown') {
+        fighter.y = fighter.y + 10
     }
 
     fighter.style.left = fighter.x + 'px'
+    fighter.style.top = fighter.y + 'px'
+
 
     // oponent
     
