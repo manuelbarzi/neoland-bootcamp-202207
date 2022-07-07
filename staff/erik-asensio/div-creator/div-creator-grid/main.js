ab011b221c14e61311125a9a11a0806c92c8c0f7
Erik Asensio <erikasensioperez@gmail.com>
2022-07-07 18:08:16 +0200

    Fixed my chess table with grid

--- staff/erik-asensio/div-creator/div-creator-grid/main.js
+++ staff/erik-asensio/div-creator/div-creator-grid/main.js
@@ -10,7 +10,6 @@ const colors=["red"," black","blue","orange","yellow","pink","violet", "gold" ,"
     for(var j = 0; j < 8 ; j++){
         for(var i = 1; i < 9 ; i++){
             var element = document.createElement("div")
-            // element.style.position="absolute"
             element.style.gridColumn= i
             element.style.width = "50px"
             element.style.height = "50px"
