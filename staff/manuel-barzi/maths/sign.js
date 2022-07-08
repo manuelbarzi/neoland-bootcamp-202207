5a09e76fac97ece16a20f04bcbd371c3899a601c
manuelbarzi <manuelbarzi@gmail.com>
2022-07-08 12:32:28 +0200

    add mod and sign challenges

---
+++ staff/manuel-barzi/maths/sign.js
@@ -0,0 +1,17 @@
+function sign(value) {
+    // TODO ...
+}
+
+// demos
+
+console.log(sign(3));
+// 1
+
+console.log(sign(-3));
+// -1
+
+console.log(sign(0));
+// 0
+
+console.log(sign('-3'));
+// -1
