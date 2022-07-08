22355edc066247813de42bd552d028a339bfcf8c
weird696969 <manuel.villalvilla@gmail.com>
2022-07-08 18:08:03 +0200

    add node ex

---
+++ staff/manuel-villalvilla/node/main.js
@@ -0,0 +1,8 @@
+console.log('hola mundo');
+
+var nombres = ['lucas', 'marta', 'pedro', 'rosa', 'antonio', 'jose', 'helena'];
+
+for (var i=0; i < nombres.length; i++)
+    console.log(nombres[i])
+
+console.log('end');
