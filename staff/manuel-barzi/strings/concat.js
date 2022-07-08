d80fcb126691b640cc5dd3c415cb33cac5bef053
manuelbarzi <manuelbarzi@gmail.com>
2022-07-08 12:59:58 +0200

    add concat and replace-char challenges

---
+++ staff/manuel-barzi/strings/concat.js
@@ -0,0 +1,14 @@
+function concat(text, text2) {
+    // TODO ...
+}
+
+// tests
+
+console.log(concat('Hola', 'Mundo'))
+// HolaMundo
+
+console.log(concat('Adios', 'Mundo', 'Cruel'))
+// AdiosMundoCruel
+
+console.log(concat('i', ' ', 'love', ' ', 'coding'))
+// i love coding
