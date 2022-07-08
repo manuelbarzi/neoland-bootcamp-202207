1165692c34c4c60f709ad5752808da3c2425d5e6
Erik Asensio <erikasensioperez@gmail.com>
2022-07-08 11:46:41 +0200

    Working with node for the first time

---
+++ staff/erik-asensio/node/main.js
@@ -0,0 +1,10 @@
+console.log("Hola mundo")
+
+var people = ["Jack", "John", "Mary", "Wendy", "Peter", "Coco"]
+
+
+for (var i = 0; i<people.length; i++){
+    console.log(people[i])
+}
+
+console.log("end")
