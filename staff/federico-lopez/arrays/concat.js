bfb9ad2e5085cb004ef761be07aea9d22d51a291
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-07-08 14:09:08 +0200

    add arrays and strings

---
+++ staff/federico-lopez/arrays/concat.js
@@ -0,0 +1,14 @@
+function concat(array, array2) {
+    // TODO ...
+}
+
+// tests
+
+console.log(concat([1, 2], [3, 4, 5]))
+// [1, 2, 3, 4, 5]
+
+console.log(concat(['h', 'o', 'l', 'a'], ['m', 'u', 'n', 'd', 'o']))
+// ['h', 'o', 'l', 'a', 'm', 'u', 'n', 'd', 'o']
+
+console.log(concat([1, 2], [3, 4], [5, 6]))
+// [1, 2, 3, 4, 5, 6]
---
+++ staff/federico-lopez/strings/count-chars.js
@@ -0,0 +1,14 @@
+function countChars(text) {
+    // TODO ...
+}
+
+// tests
+
+console.log(countChars('hola mundo'))
+// 9
+
+console.log(countChars('hello world'))
+// 10
+
+console.log(countChars('1 2 3 4 5'))
+// 9
