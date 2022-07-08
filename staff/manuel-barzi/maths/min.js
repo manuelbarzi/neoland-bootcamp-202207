c62533fbda68928464a9e75f37c269bd03ce1d67
manuelbarzi <manuelbarzi@gmail.com>
2022-07-08 12:35:32 +0200

    add min iteration

--- staff/manuel-barzi/maths/min.js
+++ staff/manuel-barzi/maths/min.js
@@ -1,5 +1,8 @@
 function min(a, b) {
-    // TODO ...
+    if (a < b)
+        return a
+    else
+        return b
 }
 
 // demos
@@ -10,5 +13,5 @@ console.log(min(4, 1))
 console.log(min(123, 456))
 // 123
 
-console.log(min(1, 2, 3, 0, 4, 5))
+console.log(min(1, 2, 3, 0, 4, 5)) // TODO learn about "js arguments"
 // 0
