0683b6c285b6d22a7b25cf01a29197b280d26c16
manuelbarzi <manuelbarzi@gmail.com>
2022-07-08 12:46:00 +0200

    add string challenges

---
+++ staff/manuel-barzi/strings/invert-case.js
@@ -0,0 +1,14 @@
+function invertCase(text) {
+    // TODO ...
+}
+
+// tests
+
+console.log(invertCase('Hello World'))
+// hELLO wORLD
+
+console.log(invertCase('a B c D e F'))
+// A b C d E f
+
+console.log('i lOVe COdInG')
+// I LovE coDiNg
