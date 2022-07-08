265dae8898c696db1bae5e8421fba873dd3a81ee
manuelbarzi <manuelbarzi@gmail.com>
2022-07-08 12:07:40 +0200

    add pow and abs scripts

---
+++ staff/manuel-barzi/maths/abs.js
@@ -0,0 +1,14 @@
+function abs(value) {
+    // TODO implement me
+}
+
+// demos
+
+console.log(abs(2 - 7))
+// 5
+
+console.log(abs(2 * 10 * -9))
+// 180
+
+console.log(abs(1.2345 - 9.8765))
+// 8.642
