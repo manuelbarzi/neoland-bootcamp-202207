75200ca87265655df2dcf1b0b165d121a83dc441
Erik Asensio <erikasensioperez@gmail.com>
2022-07-09 21:53:44 +0200

    Calculated the rest with js func

---
+++ staff/erik-asensio/maths/mod.js
@@ -0,0 +1,16 @@
+function mod(value, divisor) {
+    rest= arguments[0]%arguments[1]
+    console.log
+    return(rest)
+}
+
+// tests
+
+console.log(mod(10, 2))
+// 0
+
+console.log(mod(3, 2))
+// 1
+
+console.log(mod(11, 3))
+// 2
