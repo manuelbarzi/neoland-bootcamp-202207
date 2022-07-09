bc1882d9e6679b0e5644bf8068abe0a44585250c
Adrian <ruiz.herrera.adrian@gmail.com>
2022-07-09 20:16:39 +0200

    improve count-words code for empty strings

--- staff/adrian-ruiz/strings/count-words.js
+++ staff/adrian-ruiz/strings/count-words.js
@@ -1,20 +1,18 @@
 function countWords(text) {
-    // TODO ...
+    
+    if(text[0]=== " "){
+        spaceCount = 0;
+    }else spaceCount = 1;
 
-    // Esto es para ver como funciona split
-    /*
-    var texto = text.split(" ")
-    console.log(texto)
-
-    return text.split(" ").length;
-    */
-    var spaceCount = 0;
     for(let i = 0; i < text.length; i++){
-        if(text[i] === " "){
+        
+        if(text[i] === " " && text[i+1]!== " " && (i+1)!==text.length){
             spaceCount += 1; 
         }
+        
     }
-    return spaceCount++
+    
+    return spaceCount
 }
 
 // tests
@@ -27,3 +25,5 @@ console.log(countWords('hello world'))
 
 console.log(countWords('1 2 3 4 5'))
 // 5
+
+console.log(countWords('123   '))
