cabd18576fdbc8cbcb7c80d5e7b4f259700d009e
weird696969 <manuel.villalvilla@gmail.com>
2022-07-09 17:06:38 +0200

    edit arrays

---
+++ staff/manuel-villalvilla/arrays/map.js
@@ -0,0 +1,22 @@
+function map(mapFunction, array) {
+    if (array.length > 0) {
+        var newArray = [];
+        for (var i = 0; i < array.length; i++) {
+            newArray[i] = mapFunction(array[i]);
+        }
+        return newArray;
+    }
+}
+
+var arrayOfNumbers = [1, 2, 5, 7, 65];
+var personas = [
+    {nombre: "Luis", apellido: "Garrido"},
+    {nombre: "Helena", apellido: "Tendero"},
+    {nombre: "Julia", apellido: "Otero"}
+];
+
+console.log(map((x) => x * 2, arrayOfNumbers));
+// expected output: [2, 4, 10, 14, 130]
+
+console.log(map((x) => [x.nombre,x.apellido].join(" "), personas));
+// expected output: ['Luis Garrido', 'Helena Tendero', 'Julia Otero']
