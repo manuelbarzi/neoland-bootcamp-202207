e5c40b39becc624b5c5c147c2fcbed5c6fb18dc2
weird696969 <manuel.villalvilla@gmail.com>
2022-07-10 00:38:37 +0200

    edit strings

--- staff/manuel-villalvilla/strings/invert-case.js
+++ staff/manuel-villalvilla/strings/invert-case.js
@@ -1,12 +1,20 @@
 function invertCase(text) {
-    var abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'];
+    var abecedariom = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+    var abecedarioM = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '];
     var newText = '';
     for (var i = 0; i < text.length; i++) {
-        if (text[i] === text[i].toUpperCase()) {
-            newText += text[i].toLowerCase(); // preguntar por que no sirve editar text: text[i] = text[i].toLowerCase()
-        } else if (text[i] === text[i].toLowerCase()) {
-            newText += text[i].toUpperCase();
+        for (var z = 0; z < abecedariom.length; z++) {
+            if (text[i] === abecedariom[z]) {
+                newText += abecedarioM[z];
+            } else if (text[i] === abecedarioM[z]) {
+                newText += abecedariom[z];
+            } 
         }
+        // if (text[i] === text[i].toUpperCase()) {
+        //     newText += text[i].toLowerCase(); // preguntar por que no sirve editar text: text[i] = text[i].toLowerCase()
+        // } else if (text[i] === text[i].toLowerCase()) {
+        //     newText += text[i].toUpperCase();
+        // }
     }
     return newText;
 }
