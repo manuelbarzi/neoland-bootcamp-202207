8262a9de82d2cc4d7aa59bbe0ae0fc063aac9a3f
donyol <danieltomlinson123@gmail.com>
2022-07-09 19:35:00 +0200

    strings update without methods

--- staff/daniel-tomlinson/strings/count-words.js
+++ staff/daniel-tomlinson/strings/count-words.js
@@ -3,7 +3,15 @@ function countWords(text) {
 
   //assuming all text contains minimum 1 word
   let count = 1;
-  for (i = 0; i < text.length; i++) if (text[i] === " ") count += +1;
+  if (text.length === 0) count = 0;
+  for (i = 0; i < text.length; i++)
+    if (
+      text[i] === " " &&
+      text[i + 1] !== " "
+      // ||
+      // (text[i] === " " && i <= text.length)
+    )
+      count += +1;
 
   return count;
 }
@@ -13,8 +21,22 @@ function countWords(text) {
 console.log(countWords("adiós mundo cruel"));
 // 3
 
-console.log(countWords("hello world"));
+console.log(countWords("hello    world"));
 // 2
 
 console.log(countWords("1 2 3 4 5"));
 // 5
+
+console.log(countWords("hello   world"));
+// 2
+
+console.log(countWords("       "));
+// 0
+
+console.log(countWords(""));
+console.log(countWords(" "));
+console.log(countWords("  "));
+console.log(countWords("   "));
+console.log(countWords("    "));
+console.log(countWords("     "));
+// 0
