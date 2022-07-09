e92b0d2d9d643b7027acf4e3de542d168b0f2117
Erik Asensio <erikasensioperez@gmail.com>
2022-07-09 12:22:36 +0200

    Done my to-do with arrays

---
+++ staff/erik-asensio/arrays/concat.js
@@ -0,0 +1,17 @@
+function concat(array, array2) {
+    for (i = 0; i < array2.length; i++)
+    array.push(array2[i])
+    return array
+}
+
+
+// tests
+
+console.log(concat([1, 2], [3, 4, 5]))
+// [1, 2, 3, 4, 5]
+
+console.log(concat(['h', 'o', 'l', 'a'], ['m', 'u', 'n', 'd', 'o']))
+// ['h', 'o', 'l', 'a', 'm', 'u', 'n', 'd', 'o']
+
+console.log(concat([1, 2], [3, 4], [5, 6]))
+// [1, 2, 3, 4, 5, 6]
---
+++ staff/erik-asensio/arrays/join.js
@@ -0,0 +1,21 @@
+function join(array, separator) {
+    var frase
+    for (i =0; i < array.length; i++){
+        frase = array[i]
+        console.log(frase + arguments[1])
+    }
+}
+
+// tests;
+
+console.log(join(['Fire', 'Air', 'Water'],' '));
+// Fire Air Water
+
+console.log(join(['Fire', 'Air', 'Water'],''));
+// FireAirWater
+
+console.log(join(['Fire', 'Air', 'Water'],'-'));
+// Fire-Air-Water
+
+console.log(join(['Fire', 'Air', 'Water'],','));
+// Fire,Air,Water
