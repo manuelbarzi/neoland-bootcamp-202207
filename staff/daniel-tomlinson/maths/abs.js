6980f14c28adfb0cb802fdc5f0cf095c50ea39fa
donyol <danieltomlinson123@gmail.com>
2022-07-09 19:33:44 +0200

    maths updated without methods

--- staff/daniel-tomlinson/maths/abs.js
+++ staff/daniel-tomlinson/maths/abs.js
@@ -22,3 +22,24 @@ console.log(abs(2 * 10 * -9));
 
 console.log(abs(1.2345 - 9.8765));
 // 8.642
+
+//Absolute 2
+
+function abs1(a) {
+  if (a >= 0) return a;
+  else return a * -1;
+}
+
+// function abs2(a) {
+//   return Math.abs(a);
+// }
+
+console.log(abs1(12 * 4 + 3));
+console.log(abs1(12 * 4 + 3 - 71));
+
+console.log(abs2(12 * 4 + 3));
+console.log(abs2(12 * 4 + 3 - 71));
+
+// numero absoluto
+// -21 = 21
+//21 = 21
