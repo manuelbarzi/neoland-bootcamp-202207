0a6a11fe1959d8bc1705c9f92f32a5ce0fc7bc85
Erik Asensio <erikasensioperez@gmail.com>
2022-07-09 12:54:54 +0200

    Made my counting-chars on strings exercise

---
+++ staff/erik-asensio/strings/count-chars.js
@@ -0,0 +1,15 @@
+function countChars(text) {
+    return(text.length)
+}
+
+
+// tests
+
+console.log(countChars('hola mundo'))
+// 9
+
+console.log(countChars('hello world'))
+// 10
+
+console.log(countChars('1 2 3 4 5'))
+// 9
