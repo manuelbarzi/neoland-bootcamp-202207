b8ec683c62ca1fcb843227124693a89f65bb29c0
weird696969 <manuel.villalvilla@gmail.com>
2022-07-10 00:56:54 +0200

    edit strings

--- staff/manuel-villalvilla/strings/invert-case.js
+++ staff/manuel-villalvilla/strings/invert-case.js
@@ -1,6 +1,6 @@
 function invertCase(text) {
-    var abecedariom = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
-    var abecedarioM = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '];
+    var abecedariom = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+    var abecedarioM = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '];
     var newText = '';
     for (var i = 0; i < text.length; i++) {
         for (var z = 0; z < abecedariom.length; z++) {
