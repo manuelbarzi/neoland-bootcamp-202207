b3f38500770df33991f5c64e08959d0d18413c9d
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-11 18:40:42 +0200

    add pop testing with testy

---
+++ staff/adrian-ruiz/arrays/pop.test.js
@@ -0,0 +1,27 @@
+var arrayOfNumbers = [0, 1, 2, 3]
+
+var animals = ['dog', 'cat', 'elephant']
+
+
+describe('pop', function() {
+    
+    test('in array of numbers', function() {
+        const arrayOfNumbers = [0, 1, 2, 3]
+        
+        check(pop(arrayOfNumbers), 3)
+
+        check(arrayOfNumbers[0], 0)
+        check(arrayOfNumbers[1], 1)
+        check(arrayOfNumbers[2], 2)
+        check(arrayOfNumbers[3], undefined)
+        check(arrayOfNumbers.length, 3)
+    })
+
+    test('in array of strings', function() {
+        check(pop(animals),'elephant')
+
+        check(animals[0], 'dog')
+        check(animals[1], 'cat')
+        check(animals[2], undefined)
+    })
+})
