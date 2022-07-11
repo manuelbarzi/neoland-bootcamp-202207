dabdf80ed749050afbe06f5fa644eac8fab79646
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-11 10:36:44 +0200

    improved concat code

--- staff/adrian-ruiz/arrays/concat.js
+++ staff/adrian-ruiz/arrays/concat.js
@@ -2,6 +2,23 @@ function concat(array, array2) {
     // TODO ...
     
     var newArray = []
+    for(let i = 0; i < arguments.length; i++){
+
+        for(let j = 0; j < arguments[i].length; j++){
+        newArray[newArray.length] = arguments[i][j]
+        }
+    }
+    return newArray
+    
+}
+
+/*
+--> Esta versión está menos optimizada <--
+
+function concat(array, array2) {
+    // TODO ...
+    
+    var newArray = []
     var x = 0;
     for(let i = 0; i < arguments.length; i++){
 
@@ -14,7 +31,7 @@ function concat(array, array2) {
     return newArray
     
 }
-
+*/
 
 // tests
 
