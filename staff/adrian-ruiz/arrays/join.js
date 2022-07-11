dd15f52cd30859894f3f0d8e5a9a8fd6817dbd27
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-11 11:48:43 +0200

    fix join code

--- staff/adrian-ruiz/arrays/join.js
+++ staff/adrian-ruiz/arrays/join.js
@@ -1,6 +1,8 @@
 function join(array, separator) {
     // TODO ...
     var newText = ""
+    if(!separator)
+        separator = ","
     for(let i = 0; i < array.length; i++){
         if(i === array.length -1){
             newText = newText + array[i]
@@ -22,5 +24,5 @@ console.log(join(['Fire', 'Air', 'Water'],''));
 console.log(join(['Fire', 'Air', 'Water'],'-'));
 // Fire-Air-Water
 
-console.log(join(['Fire', 'Air', 'Water'],','));
+console.log(join(['Fire', 'Air', 'Water'],''));
 // Fire,Air,Water
