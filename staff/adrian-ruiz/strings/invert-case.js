fbefbf9d2105fd6a7419f13c6070522044bf54e3
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-11 16:46:34 +0200

    improve invert-case without methods

--- staff/adrian-ruiz/strings/invert-case.js
+++ staff/adrian-ruiz/strings/invert-case.js
@@ -1,12 +1,27 @@
 function invertCase(text) {
     // TODO ...
+
+    const alphabetLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+    const alphabetUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     var newText = "";
+
     for(var i = 0; i < text.length; i++){
-        if(text[i] === text[i].toLowerCase()){
-            newText += text[i].toUpperCase();
-        }else{
-            newText += text[i].toLowerCase();
-        }
+
+        if(text[i] === " ")
+            newText += " "
+        else{
+            for(let j = 0; j < alphabetLowerCase.length; j++){
+            
+                if(text[i] === alphabetLowerCase[j]){
+                    newText += alphabetUpperCase[j];
+                    break
+                }
+                else if(text[i] === alphabetUpperCase[j]){
+                    newText += alphabetLowerCase[j]
+                    break
+                }
+            }
+        } 
     }
     return newText
 }
@@ -21,3 +36,6 @@ console.log(invertCase('a B c D e F'))
 
 console.log(invertCase('i lOVe COdInG'))
 // I LovE coDiNg
+
+//Futuras implementaciones
+//Que devuelva también los simbolos: / ! % .....
