b2dd0319f2778ebb3f2cfb75ea628ec78715af1c
weird696969 <manuel.villalvilla@gmail.com>
2022-07-11 16:13:22 +0200

    add index.html and arr corrections

--- staff/manuel-villalvilla/arrays/concat.js
+++ staff/manuel-villalvilla/arrays/concat.js
@@ -1,22 +1,11 @@
 function concat(array, array2) {
     var newArray = [];
-    var x = 0;
+    // var x = 0;
     for (var i = 0; i < arguments.length; i++) {
         for (var j = 0; j < arguments[i].length; j++) { 
-            newArray[x] = arguments[i][j];
-            x++;
+            newArray[newArray.length] = arguments[i][j];
+            // x++;
         }
     }
     return newArray;
 }
-
-// tests
-
-console.log(concat([1, 2], [3, 4, 5]))
-// [1, 2, 3, 4, 5]
-
-console.log(concat(['h', 'o', 'l', 'a'], ['m', 'u', 'n', 'd', 'o']))
-// ['h', 'o', 'l', 'a', 'm', 'u', 'n', 'd', 'o']
-
-console.log(concat([1, 2], [3, 4], [5, 6]))
-// [1, 2, 3, 4, 5, 6]
--- staff/manuel-villalvilla/arrays/join.js
+++ staff/manuel-villalvilla/arrays/join.js
@@ -1,13 +1,28 @@
-function join(array, separator) {
-    var newString = '';
-    for (var i = 0; i < array.length; i++) {        
-        if (i === array.length - 1) {
-            newString += array[i];
-            return newString;
+// function join(array, separator) {
+//     var newString = '';
+//     for (var i = 0; i < array.length; i++) {        
+//         if (i === array.length - 1) {
+//             newString += array[i];
+//             return newString;
+//         }
+//         newString += array[i] + separator;
+//     }
+//     return newString;
+// }
+
+function join(array, separator = ',') { // valor por defecto de separator si no se envía = ","
+    let result = '';
+
+    for (let i = 0; i < array.length; i++) {
+        var element = array[i];
+
+        result += element;
+
+        if (i < array.length - 1) {
+            result += separator;
         }
-        newString += array[i] + separator;
     }
-    return newString;
+    return result;
 }
 
 // tests;
@@ -21,5 +36,5 @@ console.log(join(['Fire', 'Air', 'Water'],''));
 console.log(join(['Fire', 'Air', 'Water'],'-'));
 // Fire-Air-Water
 
-console.log(join(['Fire', 'Air', 'Water'],','));
+console.log(join(['Fire', 'Air', 'Water']));
 // Fire,Air,Water
