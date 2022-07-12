b8faa27187772417f4548f5a163f232877695511
weird696969 <manuel.villalvilla@gmail.com>
2022-07-13 01:11:55 +0200

    edit flat.js

--- staff/manuel-villalvilla/arrays/flat.js
+++ staff/manuel-villalvilla/arrays/flat.js
@@ -1,30 +1,32 @@
 function flat (array, depth) {
-    if (depth) {
-        let newArr = flat(array); // funcion recursiva
-        for (let a = 1; a < depth; a++) {
-            newArr = flat(newArr);
-        }
-        return newArr;
-    } 
-    const newArray = [];
-    let index = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (typeof array[i] === 'object') {
-            for (let j = 0; j < array[i].length; j++) {
-                if (typeof array[i][j] === 'number') {
-                    newArray[index] = array[i][j];
-                    index++;
-                } else {
-                    newArray[index] = array[i][0];
-                    index++;
-                }
+    if (Array.isArray(array)) { // compruebo si el array pasado es un array lo primero
+        if (depth && typeof depth === 'number') {
+            let newArr = flat(array); // funcion recursiva
+            for (let a = 1; a < depth; a++) {
+                newArr = flat(newArr);
             }
-        } else {
-            newArray[index] = array[i];
-            index++
+            return newArr;
+        } 
+        const newArray = [];
+        let index = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (typeof array[i] === 'object') {
+                for (let j = 0; j < array[i].length; j++) {
+                    if (typeof array[i][j] === 'number') {
+                        newArray[index] = array[i][j];
+                        index++;
+                    } else {
+                        newArray[index] = array[i][0];
+                        index++;
+                    }
+                }
+            } else {
+                newArray[index] = array[i];
+                index++
+            }
         }
+        return newArray;
     }
-    return newArray;
 }
 
 console.log(flat([0, 1, 2, [[3, 4]]]));
