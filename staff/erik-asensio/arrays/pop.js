d86126d9976bed3f2c77b31d14c594dfe0aca954
Erik Asensio <erikasensioperez@gmail.com>
2022-07-12 13:17:24 +0200

    fixed for empty arrays

--- staff/erik-asensio/arrays/pop.js
+++ staff/erik-asensio/arrays/pop.js
@@ -1,6 +1,10 @@
 function pop(array) {
     var result = array[array.length-1]
+    if (array.length > 0){
+        array.length -= 1
+
+        return result
+    }
     
-    array.length -= 1
     return result
 }
