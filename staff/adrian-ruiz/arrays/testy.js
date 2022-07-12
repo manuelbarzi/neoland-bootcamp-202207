34523d25e33f3a4f783ef7bd3800c9162cfa0ed3
Adrian <ruiz.herrera.adrian@gmail.com>
2022-07-12 11:42:44 +0200

    fix testy visual bug and improve visual printing

--- staff/adrian-ruiz/arrays/testy.js
+++ staff/adrian-ruiz/arrays/testy.js
@@ -1,13 +1,13 @@
 console.log('%cTESTY👌%cv0.0', 'color: white; font-size: 24px; font-weight: bold; background: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(0,255,0,1) 100%);', 'color: black;')
 
 function describe(description, suite) {
-    console.log('%c' + description, 'font-size: 14px; font-weigth: bold; color: blue;')
+    console.log('%c' + description, 'font-family: Verdana; font-size: 16px; font-weight: bold; color: blue; text-decoration: underline;')
 
     suite()
 }
 
 function test(description, test) {
-    console.log('%c' + description, 'font-size: 12px; font-weigth: bold; color: dodgerblue;')
+    console.log('%c' + description, 'font-size: 12px; font-weight: bold; color: dodgerblue;')
 
     test()
 }
