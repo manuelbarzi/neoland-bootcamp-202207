660cade9c9f0ce6d44fe1c7909c772e292e27233
manuelbarzi <manuelbarzi@gmail.com>
2022-07-12 11:31:08 +0200

    add more for-each demos

--- stuff/js/higher-order-functions.js
+++ stuff/js/higher-order-functions.js
@@ -24,3 +24,23 @@ forEach([[1, 2, 3], ['a', 'b', 'c'], [true, false]], function(array) {
         console.log('> ' + element)
     })
 })
+
+// #2
+
+[1, 2, 3].forEach(function(element) { console.log(element) })
+// VM8040:1 1
+// VM8040:1 2
+// VM8040:1 3
+// undefined
+function forEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i]
+
+        callback(element)
+    }
+}
+// undefined
+forEach([1, 2, 3], function(element) { console.log(element) })
+// VM8129:1 1
+// VM8129:1 2
+// VM8129:1 3
