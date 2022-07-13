12ffb81a9f36a1e1c20d5030d816b5ce0bbde44c
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-13 12:31:10 +0200

    modify function and test for filtering objects

--- staff/adrian-ruiz/arrays/filter.js
+++ staff/adrian-ruiz/arrays/filter.js
@@ -1,4 +1,4 @@
-var arrayOfNumbers = [10, 28, 17, 14, 25, 64, 15, 13, 19];
+
 
 function filter(check, array){
     let newArray = [];
@@ -10,8 +10,4 @@ function filter(check, array){
         }
     }
     return newArray
-}
-
-function checkAge(age) {
-    return age >= 18;
 }
