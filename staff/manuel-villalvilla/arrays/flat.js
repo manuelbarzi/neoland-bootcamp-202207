3b6f38d71382791fcf45d33deb25cbd3abc3f12c
weird696969 <manuel.villalvilla@gmail.com>
2022-07-13 12:31:44 +0200

    edit flat.js

--- staff/manuel-villalvilla/arrays/flat.js
+++ staff/manuel-villalvilla/arrays/flat.js
@@ -8,44 +8,44 @@ function flat (array, depth) {
             return newArr;
         } 
         const newArray = [];
-        let index = 0;
+        // let index = 0; 
         for (let i = 0; i < array.length; i++) {
             if (typeof array[i] === 'object') {
                 for (let j = 0; j < array[i].length; j++) {
                     if (typeof array[i][j] === 'number') {
-                        newArray[index] = array[i][j];
-                        index++;
+                        newArray[newArray.length] = array[i][j];
+                        // index++;
                     } else {
-                        newArray[index] = array[i][0];
-                        index++;
+                        newArray[newArray.length] = array[i][0];
+                        // index++;
                     }
                 }
             } else {
-                newArray[index] = array[i];
-                index++
+                newArray[newArray.length] = array[i];
+                // index++;
             }
         }
         return newArray;
     }
 }
 
-// console.log(flat([0, 1, 2, [[3, 4]]]));
-// // expected output [0, 1, 2, [3, 4]]
+console.log(flat([0, 1, 2, [[3, 4]]]));
+// expected output [0, 1, 2, [3, 4]]
 
-// console.log(flat([0, 1, 2, [3, 4]]));
-// // expected output [0, 1, 2, 3, 4]
+console.log(flat([0, 1, 2, [3, 4]]));
+// expected output [0, 1, 2, 3, 4]
 
-// console.log(flat([[0, 1], 2, [[3, 4]], 5, 6, [[[7, 8]]]]));
-// // expected output [0, 1, 2, [3, 4], 5, 6, [[7, 8]]]
+console.log(flat([[0, 1], 2, [[3, 4]], 5, 6, [[[7, 8]]]]));
+// expected output [0, 1, 2, [3, 4], 5, 6, [[7, 8]]]
 
-// console.log(flat([0, 1, 2, [[[3, 4]]]], 2))
-// // expected output [0, 1, 2, [3, 4]]
+console.log(flat([0, 1, 2, [[[3, 4]]]], 2))
+// expected output [0, 1, 2, [3, 4]]
 
-// console.log(flat([0, 1, 2, [[[3, 4]]]], 3))
-// // expected output [0, 1, 2, 3, 4]
+console.log(flat([0, 1, 2, [[[3, 4]]]], 3))
+// expected output [0, 1, 2, 3, 4]
 
-// console.log(flat([0, [[1, 2]], [[[3, 4]]]], 1))
-// // expected output [0, [1, 2], [[3, 4]]]
+console.log(flat([0, [[1, 2]], [[[3, 4]]]], 1))
+// expected output [0, [1, 2], [[3, 4]]]
 
 console.log(flat([0, [[1, 2]], [[[3, 4]]]], 10))
 // expected output [0, 1, 2, 3, 4]
