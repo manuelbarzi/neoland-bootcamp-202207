2befdce437001ff9ab57fa43fc2c7ca4b215c9d2
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-07-13 13:49:36 +0200

    correct spaces

--- staff/federico-lopez/arrays/sort.js
+++ staff/federico-lopez/arrays/sort.js
@@ -1,6 +1,4 @@
 function sort(array, compareFunction) {
-    debugger
-
     if(!compareFunction) {
         for (let i = 0; i < array.length - 1; i++) {
             let smallestValue, smallestIndex
