798842b788530e85eda9a432462e80574de85531
yoferpatlv <yoferpa_1995@hotmail.com>
2022-07-15 17:52:59 +0200

    add toString

--- staff/jose-fernandez/arrays/toString.js
+++ staff/jose-fernandez/arrays/toString.js
@@ -1,8 +1,11 @@
 function toString(array){
-    for(let i = 0;i<array.length;i++)
-    array[i]= ""+array[i]+""
+    let result=array[0]
+    for(let i = 0;i<array.length-1;i++)
+    result=result+' , '+ array[i+1]
 
-    return array
+    return result
 }
-let array= [1,2,3,4,5,6]
+let array= ['1','2','3','4','5','6']
+let array2= [1,2,3,4,5,6]
 console.log(toString(array))
+console.log(toString(array2))
