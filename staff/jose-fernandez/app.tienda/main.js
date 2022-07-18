3e708b9cc4708b674e1fd1440d8b31c25117a60e
yoferpatlv <yoferpa_1995@hotmail.com>
2022-07-18 23:06:10 +0200

    update with errors

--- staff/jose-fernandez/app.tienda/main.js
+++ staff/jose-fernandez/app.tienda/main.js
@@ -9,3 +9,27 @@ btnMenu.addEventListener('click', function () {
 const loginPage = document.querySelector('.login_page')
 const registerPage = document.querySelector('.register_page')
 const homePage = document.querySelector('.home_page')
+
+const btn_login = homePage.querySelector('.btn__login')
+btn_login.onclick=function(event){
+    event.preventDefault()
+
+    homePage.classList.add('off')
+    loginPage.classList.remove('off')
+}
+
+const registerLink = loginPage.querySelector('.link-register')
+registerLink.onclick=function(event){
+    event.preventDefault()
+
+    loginPage.classList.add('off')
+    registerPage.classList.remove('off')
+}
+
+const loginLink=registerPage.querySelector('.link-login')
+loginLink.onclick=function(event){
+event.preventDefault()
+
+    registerPage.classList.add('off')
+    loginPage.classList.remove('off')
+}
