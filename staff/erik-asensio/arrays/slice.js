260acc16e495dd65f3e3b8781e1799d9ce6579ee
Erik Asensio <erikasensioperez@gmail.com>
2022-07-18 09:42:27 +0200

    renamed the result variable, sliced -> result

--- staff/erik-asensio/arrays/slice.js
+++ staff/erik-asensio/arrays/slice.js
@@ -1,23 +1,23 @@
-function slice(array, inicio, final){
+function slice(array, inicio, final) {
     let result = []
 
-    if(inicio == undefined && final == undefined){
-        for(let i = 0; i < array.length; i++){
+    if (inicio == undefined && final == undefined) {
+        for (let i = 0; i < array.length; i++) {
             result[result.length] = array[i]
         }
         return result
     }
 
-    if(final < 0){
+    if (final < 0) {
         final = array.length + final
-    }else if(final == undefined){
+    } else if (final == undefined) {
         final = array.length
     }
-    if(inicio < 0){
+    if (inicio < 0) {
         inicio = array.length + inicio
     }
 
-    for(let i = inicio; i < final; i++){
+    for (let i = inicio; i < final; i++) {
         result[result.length] = array[i]
     }
 
