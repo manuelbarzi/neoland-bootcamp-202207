14a1c9c040db4a3af124e7262f1a0b0e5fcbdd4b
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-18 18:53:50 +0200

    add old versions of app

---
+++ staff/adrian-ruiz/app.2/data.js
@@ -0,0 +1,13 @@
+const users = []
+    
+    
+users.push(
+    {name:'Adrian', email: 'ruiz.herrera.adrian@gmail.com', password:'123123123'}
+)
+users.push( 
+    {name: 'Pepito', email: 'pepito@pepito.com', password:'123123123'}
+    )
+
+users.push(
+    {name:'Test', email:'test@test.com', password: '123123123'}
+)
