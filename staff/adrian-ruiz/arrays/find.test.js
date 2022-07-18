0f0fb6b18f290d2926ae2dceb4b6e5915ebb522b
rakyrion <ruiz.herrera.adrian@gmail.com>
2022-07-18 19:19:13 +0200

    updated test for objects

--- staff/adrian-ruiz/arrays/find.test.js
+++ staff/adrian-ruiz/arrays/find.test.js
@@ -64,4 +64,32 @@ describe('TESTING find in Arrays', function(){
         check(result,44)
 
     })
+
+    test('find an user that has 1234 id', function() {
+        const users = [
+            { name: 'carlos', id: 1234 },
+            { name: 'pepito', id: 1978 }
+        ]
+    
+        const userFound = find(users, function (user) {
+            return user.id === 1234
+        })
+        
+        check(userFound instanceof Object, true)
+        check(userFound, users[0])
+        check(userFound.name, 'carlos')
+    })
+
+    test('on unexisting user with id 1999', function() {
+        const users = [
+            { name: 'carlos', id: 1234 },
+            { name: 'pepito', id: 1978 }
+        ]
+    
+        const userFound = find(users, function (user) {
+            return user.id === 1999
+        })
+        
+        check(userFound, undefined)
+    })
 })
