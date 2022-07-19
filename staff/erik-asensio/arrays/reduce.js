81c9cd4a93952a5a5545c26c66f71144e58add2a
Erik Asensio <erikasensioperez@gmail.com>
2022-07-19 15:34:30 +0200

    Made the reduce method

--- staff/erik-asensio/arrays/reduce.js
+++ staff/erik-asensio/arrays/reduce.js
@@ -1,5 +1,9 @@
-function reduce(array, callback){
-    const result = callback(array)
-    
+function reduce(array, initialValue, callback) {
+    let result = ""
+    for (let i = 0; i < array.length; i++) {
+        
+        result = callback(initialValue, array[i])
+        initialValue = result
+    }
     return result
 }
--- staff/erik-asensio/arrays/reduce.test.js
+++ staff/erik-asensio/arrays/reduce.test.js
@@ -1,15 +1,16 @@
 describe("reduce", function(){
 
     test("suma de los elementos", function(){
-        const array=[1, 2, 3, 4]
-        function sumaTotal(element){
-            let result = element[0]
-            for(let i = 1; i < array.length; i++){
-                result += element[i]
-            }
+        
+        const array = [1, 2, 3, 4]
+        const initialValue = 0
+        function sumaTotal(initialValue, element){
+            
+            let result = initialValue + element
+
             return result
         }
-        const result = reduce(array, sumaTotal)
+        const result = reduce(array, initialValue, sumaTotal)
 
         check(result, 10)
         check(typeof result, "number")
@@ -18,14 +19,14 @@ describe("reduce", function(){
     test("multiplicacion de los elementos", function(){
         
         const array=[1, 2, 3, 4]
-        function multiTotal(element){
-            let result = element[0]
-            for(let i = 1; i < array.length; i++){
-                result *= element[i]
-            }
+        const initialValue = 1
+        function multiTotal(initialValue, element){
+            
+            let result = initialValue * element
+            
             return result
         }
-        const result = reduce(array, multiTotal)
+        const result = reduce(array, initialValue, multiTotal)
 
         check(result, 24)
         check(typeof result, "number")
@@ -34,14 +35,13 @@ describe("reduce", function(){
     test("resta de los elementos", function(){
         
         const array=[1, 2, 3, 4]
-        function multiTotal(element){
-            let result = element[0]
-            for(let i = 1; i < array.length; i++){
-                result -= element[i]
-            }
+        const initialValue = 2
+        function multiTotal(initialValue, element){
+            
+            let result = initialValue - element
             return result
         }
-        const result = reduce(array, multiTotal)
+        const result = reduce(array, initialValue, multiTotal)
 
         check(result, -8)
         check(typeof result, "number")
