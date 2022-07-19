69c3e6e588b66c96df42a17b0ae61a67d6bfb70f
weird696969 <manuel.villalvilla@gmail.com>
2022-07-19 18:38:14 +0200

    add retrieveUser.spec

--- staff/manuel-villalvilla/app/logic/retrieveUser.spec.js
+++ staff/manuel-villalvilla/app/logic/retrieveUser.spec.js
@@ -53,7 +53,7 @@ describe('retrieveUser', () => {
         })
     })
 
-    it('should fail on incorrect email structure provided', () => { // unhappy path :(
+    it('should fail on invalid email | example with try catch', () => { // unhappy path :(
         const name = 'Ele Fante'
         const email = 'ele@fante.com'
         const password = '123123123'
@@ -63,6 +63,7 @@ describe('retrieveUser', () => {
         //     email: email,
         //     password: password
         // }
+
         // lo de arriba equivale a esto:
         const elefante = {
             name,
@@ -73,17 +74,64 @@ describe('retrieveUser', () => {
         users.push(elefante)
 
         try {
-            retrieveUser('ele@fante.us', (error, user) => {
+            retrieveUser('elefante', (error, user) => {
                 expect(error).toBeInstanceOf(Error)
                 expect(error.message).toBe('user with email ele@fante.us not found')
     
                 expect(user).toBe(undefined)
             })
         }
-        catch {
+        catch(error) {
             expect(error).toBeInstanceOf(Error)
             expect(error.message).toBe('email is not valid')
         }
     })
 
+    it('should fail on invalid email | example with fail', () => { // unhappy path 😦
+        const name = 'Zor Ro'
+        const email = 'zor@ro.com'
+        const password = '123123123'
+
+        const zorRo = {
+            name,
+            email,
+            password
+        }
+
+        users.push(zorRo)
+
+        const invalidEmail = 'zor_ro.com'
+
+        try {
+            retrieveUser(invalidEmail, (error, user) => {
+                // si el codigo llegó hasta aquí, jasmine lanza un error para indicar que no debería haber llegado hasta ahí
+                fail('it should not reach this point') 
+            })
+        } catch(error) {
+            expect(error).toBeInstanceOf(Error)
+            expect(error.message).toBe('email is not valid')
+        }
+    })
+
+    it('should fail on invalid email | example with throw', () => { // unhappy path 😦
+        const name = 'Zor Ro'
+        const email = 'zor@ro.com'
+        const password = '123123123'
+
+        const zorRo = {
+            name,
+            email,
+            password
+        }
+
+        users.push(zorRo)
+
+        const invalidEmail = 'zor_ro.com'
+
+        // importante para el metodo toThrow el añadir una funcion anonima en el expect que llame a lo que quieras checkear
+        expect(function() {retrieveUser(invalidEmail, (error, user) => {
+            fail('it should not reach this point')
+        })}).toThrow()
+    })
 })
+
