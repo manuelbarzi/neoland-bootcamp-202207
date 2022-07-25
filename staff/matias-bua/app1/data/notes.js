10b70903f16b0e09c4d413159dffd4d783498766
Matias N. Bua <buamatias@hotmail.com>
2022-07-25 08:46:36 +0200

    add page with addnote,deletenote & updatenote

---
+++ staff/matias-bua/app1/data/notes.js
@@ -0,0 +1,31 @@
+const notes = [];
+
+notes.push({
+  id: "note-1",
+  text: "Hola, Mundo!",
+  user: "user-1",
+});
+
+notes.push({
+  id: "note-2",
+  text: "Hello, World!",
+  user: "user-1",
+});
+
+notes.push({
+  id: "note-3",
+  text: "Ciao, Mondo!",
+  user: "user-1",
+});
+
+notes.push({
+  id: "note-4",
+  text: "Pryvit, Svit!",
+  user: "user-1",
+});
+
+notes.push({
+  id: "note-5",
+  text: "one day i will catch you, peter...",
+  user: "user-4",
+});
---
+++ staff/matias-bua/app1/logic/retrieveUser.test.js
@@ -0,0 +1,86 @@
+describe("retrieveUser", () => {
+  beforeEach(() => {
+    users.length = 0;
+  });
+
+  it("succeeds on correct email", () => {
+    // happy path :)
+    const name = "Lan Gosta";
+    const email = "lan@gosta.com";
+    const password = "123123123";
+
+    const lanGosta = {
+      name,
+      email,
+      password,
+    };
+
+    users.push(lanGosta);
+
+    retrieveUser(email, (error, user) => {
+      expect(error).toBeNull();
+
+      expect(user).toBeDefined();
+      expect(user.name).toBe(name);
+      expect(user.email).toBe(email);
+      expect(user.password).toBe(password);
+    });
+  });
+
+  it("fails on incorrect email", () => {
+    // unhappy path :(
+    const name = "Zor Ro";
+    const email = "zor@ro.com";
+    const password = "123123123";
+
+    const zorRo = {
+      name,
+      email,
+      password,
+    };
+
+    users.push(zorRo);
+
+    const wrongEmail = "zor@ro.us";
+
+    retrieveUser(wrongEmail, (error, user) => {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe(
+        "user with email " + wrongEmail + " not found"
+      );
+
+      expect(user).toBeUndefined();
+    });
+  });
+
+  it("fails on invalid email", () => {
+    // unhappy path :(
+    const name = "Zor Ro";
+    const email = "zor@ro.com";
+    const password = "123123123";
+
+    const zorRo = {
+      name,
+      email,
+      password,
+    };
+
+    users.push(zorRo);
+
+    const invalidEmail = "zor_ro.com";
+
+    try {
+      retrieveUser(invalidEmail, (error, user) => {
+        expect(error).toBeInstanceOf(Error);
+        expect(error.message).toBe(
+          "user with email " + invalidEmail + " not found"
+        );
+
+        expect(user).toBeUndefined();
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe("email is not valid");
+    }
+  });
+});
